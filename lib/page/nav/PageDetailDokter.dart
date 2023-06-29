b0589b05e4f91c69d7634a32e7f714572cbee402
Greek-Cp <82995911+Greek-Cp@users.noreply.github.com>
2023-06-29 23:30:35 +0700

    Page Detail Dokter.

--- lib/page/nav/PageDetailDokter.dart
+++ lib/page/nav/PageDetailDokter.dart
@@ -1,4 +1,5 @@
 import 'package:fit_help_app/component/ComponentText.dart';
+import 'package:fit_help_app/util/size.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -16,10 +17,60 @@ class _PageDetailDokterState extends State<PageDetailDokter> {
       body: ScreenUtilInit(
         builder: (context, child) {
           return Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              ComponentTextPrimaryTittleBold(
-                teks: "Dr. Silvia Adreno",
-              )
+              Stack(
+                children: [
+                  Column(
+                    children: [
+                      ComponentTextPrimaryTittleBold(
+                        teks: "Dr. Silvia Adreno",
+                        textAlign: TextAlign.start,
+                        colorText: Colors.white,
+                      ),
+                      ComponentTextPrimaryDescriptionRegular(
+                        teks: "Dokter Spesialis Anak",
+                        textAlign: TextAlign.start,
+                        colorText: Colors.white,
+                      ),
+                      Container(
+                        width: 100,
+                        height: 30,
+                        child: Card(
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(30)),
+                          color: Colors.white,
+                          child: Row(children: [
+                            Padding(
+                              padding: EdgeInsets.only(left: 10, right: 5),
+                              child: CircleAvatar(
+                                radius: 5,
+                                backgroundColor: Colors.green,
+                              ),
+                            ),
+                            ComponentTextPrimaryDescriptionRegular(
+                              teks: "Online",
+                              size: SizeApp.SizeTextDescription.sp,
+                            )
+                          ]),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Positioned(
+                    bottom: 1,
+                    right: 1,
+                    left: 1,
+                    child: Image.asset(
+                      "assets/gambar_halaman_awal.png",
+                      width: 270.w,
+                      height: 100.h,
+                      fit: BoxFit.cover,
+                    ),
+                  )
+                ],
+              ),
             ],
           );
         },
